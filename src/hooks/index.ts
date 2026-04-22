b776101fc92e196a1f4dc77faa3ceaf31dd49583
LJCognitix <contact@lj-cognitix.com>
2026-04-22 13:11:23 +0200

    Create index.ts with barrel exports for custom hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+// Custom hooks export barrel
+// Add your custom hooks here as they grow
+
+export { default as useCrm } from './useCrm';
+export { default as useBilling } from './useBilling';
+export { default as useTheme } from './useTheme';
